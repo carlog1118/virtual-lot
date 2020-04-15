28eda452e9c330914dfc70b4538880fd2ecf9b3e
Carlo Giustozzi <carlog1118@gmail.com>
2020-04-15 16:03:28 -0400

    added Hero to Landing Page

---
+++ src/Components/LandingPage/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hero.css';
+
+class Hero extends Component {
+  render(){
+    return <>
+      <section>
+        <h1>Virtual Lot</h1>
+        <p>Auto Inventory Made Easy</p>
+      </section>
+    </>
+  }
+}
+
+export default Hero;
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Hero from './Hero/Hero';
 
 class LandingPage extends Component {
-    render(){
-        return <>
-            <p>Lorem Ipsum</p>
-        </>
-    }
+  
+  render(){
+    return <>
+      <Hero/>
+    </>
+  }
 }
 
 export default LandingPage;
