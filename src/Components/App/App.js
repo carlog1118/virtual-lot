783626c1028b6585272b2e80e38b15f980a647bd
Carlo Giustozzi <carlog1118@gmail.com>
2020-04-15 15:35:06 -0400

    added Header component and init Router

---
+++ src/Components/App/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const UNITS = [
+  {
+    "year": "2015",
+    "make": "Honda",
+    "model": "Civic",
+    "trim": "EX Coupe",
+    "vin": "123456789abcdefg123",
+    "mileage": "75,000",
+    "color": "Red",
+    "sale price": "$10,000",
+    "cost": "$7000",
+    "status": "Available"
+  },
+  {
+    "year": "2017",
+    "make": "Toyota",
+    "model": "Avalon",
+    "trim": "XLE",
+    "vin": "123456789abcdefg124",
+    "mileage": "50,000",
+    "color": "Blue",
+    "sale price": "$20,000",
+    "cost": "$16000",
+    "status": "Available"
+  }
+]
+
+
+function App() {
+  console.log(UNITS);
+  return (
+    <main className="App">
+      <Header/>
+    </main>
+  );
+}
+
+export default App;
