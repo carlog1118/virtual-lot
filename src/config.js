9cdaa42ec30f70ee4006b332d1cbbd0715742e31
Carlo Giustozzi <carlog1118@gmail.com>
2020-04-26 23:24:22 -0400

    pd

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT ||'http://localhost:8000/api/units',
-    LOG_IN_ENDPOINT: process.env.REACT_API_LOG_IN_ENDPOINT||'http://localhost:8000/api/auth',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT ||'https://morning-beyond-84622.herokuapp.com/api/units',
+    LOG_IN_ENDPOINT: process.env.REACT_API_LOG_IN_ENDPOINT||'https://morning-beyond-84622.herokuapp.com/api/auth',
     TOKEN_KEY: '',
 }
